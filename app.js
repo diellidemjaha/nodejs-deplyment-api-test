9ee2abaad98a35976585d38795c9da9423c8d4d5
Dielli Demjaha <diellidemjaha@gmail.com>
2025-11-19 15:10:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"]
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.get('/api/info', (req, res) => {
